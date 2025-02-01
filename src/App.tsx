22c790fb45adc28c0f5a11595e5953c72ee3f578
shitoletushar3132 <shitoletushar3132@gmail.com>
2025-02-01 13:22:05 +0530

    all setup ready

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
+      <p className="text-red-600">Service Discovery Start</p>
+    </>
+  );
+}
+
+export default App;
